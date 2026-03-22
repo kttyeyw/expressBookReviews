e4a9e68def169aee7128b23128f4269f541f0d19
kttyeyw <lakeevaekaterina9@gmail.com>
2026-03-23 01:05:23 +0300

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
+const app = express();
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
