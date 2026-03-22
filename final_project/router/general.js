3d6dc2006f56dc38debd8d373e87bfa27f83c9a2
kttyeyw <lakeevaekaterina9@gmail.com>
2026-03-23 01:36:38 +0300

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,13 +18,21 @@ public_users.get('/', function(req, res) {
        return res.status(404).json({error: "there are no books"});
     }
 
-  //Write your code h
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+if(books){  
+    if(books[isbn]){
+        return res.send(JSON.stringify(books[isbn], null,4))
+    } else{
+        return res.status(404).json({error: "there are no books data"});
+
+    }}else{
+        return res.status(404).json({error: "there are no books"});
+
+    }
  });
   
 // Get book details based on author
