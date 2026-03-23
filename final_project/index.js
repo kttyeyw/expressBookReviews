36555a061e69b76d0773516d9bbae6668a21d277
kttyeyw <lakeevaekaterina9@gmail.com>
2026-03-23 19:24:23 +0300

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -5,28 +5,34 @@ const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
-const SECRET_KEY = 'secret_key'
+
+
 app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-const token = req.session.token; // Retrieve token from session
+app.use("/customer/auth/*", function auth(req, res, next) {
+  // get token from the request headers
+  const token = req.headers['authorization'];
 
-    if (!token) {
-        return res.status(401).json({ message: "Access denied. No token provided." });
+  if (!token) {
+    return res.status(403).json({ message: "Access denied. No token provided." });
+  }
+
+  //check if the token is valid
+  jwt.verify(token, 'access', (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Invalid token." });
     }
 
-    try {
-        // Verify the token
-        const decoded = jwt.verify(token, SECRET_KEY);
-        req.user = decoded; // Attach decoded payload to request
-        next();
-    } catch (err) {
-        res.status(403).json({ message: "Invalid token." });
-    }});
+    // if the token is valid, save the decoded user information to the request object
+    req.user = decoded;
+    next();  // pass control to the next middleware
+  });
+});
+
  
-const PORT =5000;
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
